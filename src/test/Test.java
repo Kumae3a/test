3f4e6211fd62d2d373c9c5c5571fdb4ef1c221d4
blokey <kumae3a@gmail.com>
2025-02-06 10:05:29 +0700

    first

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+
+package test;
+
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + 1 + " step");
+            
+        }
+    }
+    
+}
